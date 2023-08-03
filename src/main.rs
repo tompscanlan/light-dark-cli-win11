9197f6522bcd031b44a50c9e0770630f729744cf
Tom Scanlan <tompscanlan+git@gmail.com>
2023-08-03 13:47:47 -0400

    read write to our key

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,20 @@
-fn main() {
-    println!("Hello, world!");
+use std::io;
+use winreg::enums::*;
+use winreg::RegKey;
+
+fn main() -> io::Result<()> {
+    println!("Reading some system info...");
+    let hkcu = RegKey::predef(HKEY_CURRENT_USER);
+    let personalize = hkcu.open_subkey_with_flags("SOFTWARE\\Microsoft\\Windows\\CurrentVersion\\Themes\\Personalize", KEY_READ | KEY_WRITE )?;
+    let uselt: u32 =personalize.get_value("AppsUseLightTheme")?;
+    println!("AppsUseLightTheme is currently = {}", uselt);
+
+    let val: u32 = if uselt > 0 { 0u32} else { 1u32};
+
+    personalize.set_value("AppsUseLightTheme", &val)?;
+
+    let uselt: u32 =personalize.get_value("AppsUseLightTheme")?;
+    println!("Setting AppsUseLightTheme = {}", uselt);
+    
+    Ok(())
 }
