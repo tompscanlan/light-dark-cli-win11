c2009d0958582c1f4a4a0b1192a71c91f8aa7298
Tom Scanlan <tompscanlan+git@gmail.com>
2023-08-04 12:20:50 -0400

    version bump

--- src/main.rs
+++ src/main.rs
@@ -3,18 +3,21 @@ use winreg::enums::*;
 use winreg::RegKey;
 
 fn main() -> io::Result<()> {
-    println!("Reading some system info...");
     let hkcu = RegKey::predef(HKEY_CURRENT_USER);
-    let personalize = hkcu.open_subkey_with_flags("SOFTWARE\\Microsoft\\Windows\\CurrentVersion\\Themes\\Personalize", KEY_READ | KEY_WRITE )?;
-    let uselt: u32 =personalize.get_value("AppsUseLightTheme")?;
-    println!("AppsUseLightTheme is currently = {}", uselt);
+    let personalize_key_result = hkcu.open_subkey_with_flags(
+        "SOFTWARE\\Microsoft\\Windows\\CurrentVersion\\Themes\\Personalize",
+        KEY_READ | KEY_WRITE,
+    );
 
-    let val: u32 = if uselt > 0 { 0u32} else { 1u32};
+    let personalize_key = match personalize_key_result {
+     Ok(p) => p, 
+     Err(why) => panic!("Failed to open registry key for personalization: {:?}", why)   
+    };
 
-    personalize.set_value("AppsUseLightTheme", &val)?;
+    // get the setting, toggle it between 0 and 1
+    let uselt: u32 = personalize_key.get_value("AppsUseLightTheme")?;
+    let val: u32 = if uselt > 0 { 0u32 } else { 1u32 };
+    personalize_key.set_value("AppsUseLightTheme", &val)?;
 
-    let uselt: u32 =personalize.get_value("AppsUseLightTheme")?;
-    println!("Setting AppsUseLightTheme = {}", uselt);
-    
     Ok(())
 }
